57b80175036bb67c8d51d55914e76197a7dd4413
Cristiano Becker <cristianobecker@gmail.com>
2015-04-04 15:23:14 -0300

    fixing bugs, ignoring ctrl+c and removing fullscreen

--- bin/prjk.sh
+++ bin/prjk.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# variables
-
-WHILE_HANDLER=0
-
-
-# functions
-
 _prjk_alias() {
-    local COMMAND='prjk'
-    local FOLDER=`cd $2 && pwd`
+    local folder=`cd $2 && pwd`
     
-    echo "alias $1=\". $COMMAND go $FOLDER\"" >> ~/.bash_profile
+    echo "alias $1=\". prjk go $folder\"" >> ~/.bash_profile
     source ~/.bash_profile
     
     _prjk_unset
@@ -23,113 +15,122 @@ _prjk_find() {
         if test ${#LIST[@]} -gt 1; then
             _prjk_options $1
         elif test ${#LIST[@]} -eq 1; then
-            _prjk_unset
-            cd "$1/${LIST[0]}"
+            _prjk_cd "$1/${LIST[0]}"
         fi
     else        
-        _prjk_unset
-        cd $1
+        _prjk_cd $1
     fi
 }
 
 _prjk_help() {
     echo 'help'
+    _prjk_unset
 }
 
 _prjk_options() {
     _prjk_fullscreen
     _prjk_select 0
 
-    trap _prjk_sigint INT
-    trap _prjk_sigtstp SIGTSTP
-
-    WHILE_HANDLER=0
-    local UP=$'\033[A'
-    local DOWN=$'\033[B'
-    local SELECTED=0
+    local up=$'\033[A'
+    local down=$'\033[B'
+    local selected=0
+    local total=${#LIST[@]}
     
-    while test $WHILE_HANDLER -eq 0; do
-        read -n 3 c
+    #trap _prjk_sigint INT
+    trap '' INT
+
+    while read -n 3 c; do
         case $c in
-            $UP)
-                clear
-                let SELECTED=`expr $SELECTED - 1`
-                _prjk_select $SELECTED
+            $up)
+                let selected=`expr $selected - 1`
+                if test $selected -lt 0; then let selected=0; fi
+                
+                _prjk_select $selected
 	    	    ;;
-            $DOWN)
-                clear
-                let SELECTED=`expr $SELECTED + 1`
-                _prjk_select $SELECTED
+            $down)
+                let selected=`expr $selected + 1`
+                if test `expr $selected + 1` -ge $total; then let selected=`expr $total - 1`; fi
+                
+                _prjk_select $selected
                 ;;
             *)
-                _prjk_leave
-                WHILE_HANDLER=1
+                break
                 ;;
         esac
     done
+   
+    _prjk_leave
     
-    if test $WHILE_HANDLER -eq 1; then 
+    if test $selected -gt -1; then
+        _prjk_cd "$1/${LIST[$selected]}" 
+    else 
+        echo "cancel"
         _prjk_unset
-        cd "$1/${LIST[$SELECTED]}"
     fi
 }
 
-_prjk_select() {
-    echo $1
+_prjk_select() { 
     local n=0
     for folder in ${LIST[@]}; do
         if test $n -eq $1; then
-            printf "  \033[36mο\033[0m $folder\033[0m\n"
+            printf "  \033[32m√\033[0m $folder\033[0m\n"
         else
             printf "    \033[90m$folder\033[0m\n"
         fi
         let n=n+1
     done
-    echo
+    tput cuu ${#LIST[@]}
 }
 
 _prjk_filter() {
+    local oldifs="$IFS"
     IFS=$'\n'
     LIST=(`ls -1 $1 | egrep -i $2`)
+    IFS="$oldifs"
+}
+
+_prjk_cd() {
+    local list=$LIST
+    _prjk_unset
+
+    echo "cd $1"
+    
+    cd $1 
 }
 
 _prjk_fullscreen() {
-    tput smcup
+    tput civis
     stty -echo
 }
 
 _prjk_leave() {
-    tput rmcup
+    tput cud ${#LIST[@]}
+    tput cnorm
     stty echo 
 }
 
-_prjk_sigint() {
-    _prjk_leave
-    trap - INT
-    WHILE_HANDLER=2
-}
+#_prjk_sigint() {
+#    _prjk_leave
+#    echo 'sigint'
+#}
 
-_prjk_sigtstp() {
-    _prjk_leave
-    _prjk_unset
-    kill -s SIGSTOP $$
-}
 
 _prjk_unset() {
+    trap - INT
+    
     unset _prjk_filter
+    unset _prjk_cd
     unset _prjk_find
     unset _prjk_help
     unset _prjk_options
-    unset _prjk_sigint
-    unset _prjk_sigtstp
+    #unset _prjk_sigint
     unset _prjk_alias
     unset _prjk_fullscreen
     unset _prjk_leave
     unset _prjk_select
     unset _prjk_unset
-    
-    unset IFS
-    unset WHILE_HANDLER
+   
+    unset LIST 
 }
 
 
@@ -140,5 +141,6 @@ if test $# -gt 0; then
         create|alias) _prjk_alias $2 $3 ;;
         go|find) _prjk_find $2 $3 ;;
         help) _prjk_help ;;
+        *) _prjk_unset ;;
     esac
 fi 
